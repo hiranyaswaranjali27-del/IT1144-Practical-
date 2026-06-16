e9b4893d9ea992ef5aa40806ddebd780bfa48b22
hiranyaswaranjali27-del <hiranyaswaranjali27@gmail.com>
2026-06-17 00:59:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const title=document.getElementById("title");
-const button=document.
-getElementById("changeBtn");
-button.addEventListener("click",function(){
-	title.textContent="boutton clicked!";
-	
-	title.style.color="red";
-});
+function submitForm(){
+    alert("✅ Message sent successfully! We will reply soon.");
+}
+function showDetails(event){
+
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    document.getElementById("output").innerHTML = `
+        <h3>Submitted Details</h3>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong> ${message}</p>
+    `;
+}
